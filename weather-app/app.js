339d715d974d6142d1d3327f1c26b98bc1e31020
ngaruko <ngarukob@yahoo.com>
2017-11-15 16:55:40 +1300

    yargs and encode uri

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,32 @@
-const request = require('request');
+const request = require('request'),
+yargs=require('yargs');
+const argv = yargs
+.options({
+	a: {
+		demand: true,
+		alias: 'address',
+		describe: 'Address to search weather for',
+		string: true
+	}
 
+})
+.help()
+.alias('help','h')
+.argv;
+
+console.log('Address input: ' + argv.a);
 
 request({
-	url:'https://maps.googleapis.com/maps/api/geocode/json?address=33%20scaret%20oak%20drive%20auckland',
+	url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
 	json:true
 },(error, response, body)=>{
 	if(error){
 		console.log('Error occured');
 	} else {
 		console.log('Response: ' + response.statusCode);
-console.log(`Address: ${body.results[0].formatted_address}`);
+console.log(`Address received: ${body.results[0].formatted_address}`);
+//console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+//console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
 	}
 
 });
